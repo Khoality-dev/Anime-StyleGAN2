ed8d24e941e887971b81177e23f61d2ea2667443
akn0717 <magicmonster17@gmail.com>
2023-05-11 18:14:47 -0700

    add mixed precision experiment

--- train.py
+++ train.py
@@ -46,6 +46,8 @@ def train(mainWindow, args):
         
     G = G.to(DEVICE)
     D = D.to(DEVICE)
+    scaler_G = torch.cuda.amp.GradScaler()
+    scaler_D = torch.cuda.amp.GradScaler()
     print("Iteration:",G.iteration)
     print("Learning rate: ", LEARNING_RATE, end='\n\n')
 
@@ -61,30 +63,34 @@ def train(mainWindow, args):
                 reals_list.extend(list(real_samples_copy))
                 real_samples = real_samples.permute(0,3,1,2) / 127.5 - 1
                 z = torch.randn(size = (mini_batch_size, LATENT_SIZE))
-                if (G.iteration % D_LAZY_REG_FACTOR == 0):
-                    d_loss = D_loss_r1(G, D, z.to(DEVICE), real_samples.to(DEVICE), regularization=True)
-                else:
-                    d_loss = D_loss_r1(G, D, z.to(DEVICE), real_samples.to(DEVICE), regularization=False)
+                with torch.cuda.amp.autocast():
+                    if (G.iteration % D_LAZY_REG_FACTOR == 0):
+                        d_loss = D_loss_r1(G, D, z.to(DEVICE), real_samples.to(DEVICE), regularization=True)
+                    else:
+                        d_loss = D_loss_r1(G, D, z.to(DEVICE), real_samples.to(DEVICE), regularization=False)
 
-                d_loss = d_loss / GRAD_ACCUMULATE_FACTOR
-                d_loss.backward()
-
-            optimizer_D.step()
+                    d_loss = d_loss / GRAD_ACCUMULATE_FACTOR
+                scaler_D.scale(d_loss).backward()
+            
+            scaler_D.step(optimizer_D)
+            scaler_D.update()
        
         G.zero_grad()
         D.zero_grad()
         for _ in range(GRAD_ACCUMULATE_FACTOR):
             z = torch.randn(size = (mini_batch_size, LATENT_SIZE))
             g_Loss = None
-            if (G.iteration % G_LAZY_REG_FACTOR == 0):
-                g_Loss = G_loss_pl(G, D, z.to(DEVICE), regularization=True)
-            else:
-                g_Loss = G_loss_pl(G, D, z.to(DEVICE), regularization=False)
+            with torch.cuda.amp.autocast():
+                if (G.iteration % G_LAZY_REG_FACTOR == 0):
+                    g_Loss = G_loss_pl(G, D, z.to(DEVICE), regularization=True)
+                else:
+                    g_Loss = G_loss_pl(G, D, z.to(DEVICE), regularization=False)
 
-            g_Loss = g_Loss / GRAD_ACCUMULATE_FACTOR
-            g_Loss.backward()
+                g_Loss = g_Loss / GRAD_ACCUMULATE_FACTOR
+            scaler_G.scale(g_Loss).backward()
 
-        optimizer_G.step()
+        scaler_G.step(optimizer_G)
+        scaler_G.update()
 
         if (G.iteration % args.log_iter == 0):
             sys.stdout.write("\033[K")
