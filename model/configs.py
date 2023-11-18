90af474cbf6d2f5e8b08c06699dbe35e9757ce98
Khoa <anhkhoan@sfu.ca>
2023-11-17 19:52:49 -0800

    refactor and change lr to 0.0002

--- model/configs.py
+++ model/configs.py
@@ -1,18 +1,28 @@
 import torch
 
 # Hyperparameters
-RANDOM_SEED = 1.048596 # set to time(0) for none seed
+RANDOM_SEED = 1.048596  # set to time(0) for none seed
 H, W = 256, 256
 BATCH_SIZE = 32
-VISUALIZATION_BATCH_SIZE = int(5*5)
-LEARNING_RATE = 2e-5
+VISUALIZATION_BATCH_SIZE = int(5 * 5)
+LEARNING_RATE = 0.0002
 GRAD_ACCUMULATE_FACTOR = 8  # minibatch_size = BATCH_SIZE / GRAD_ACCUMULATE_FACTOR
-G_LAZY_REG_FACTOR = 4 # apply once every 4 minibatch
-D_LAZY_REG_FACTOR = 8 # apply once every 8 minibatch
+G_LAZY_REG_FACTOR = 4  # apply once every 4 minibatch
+D_LAZY_REG_FACTOR = 8  # apply once every 8 minibatch
 N_CRITICS = 1
 
 # Generator and Discriminator
-NUM_FEATURE_MAP = {512:64, 256: 64, 128:128, 64:256, 32:512, 16:512, 8:512, 4:512, 2:512}
+NUM_FEATURE_MAP = {
+    512: 64,
+    256: 64,
+    128: 128,
+    64: 256,
+    32: 512,
+    16: 512,
+    8: 512,
+    4: 512,
+    2: 512,
+}
 NUM_MAPPING_LAYER = 4
 LATENT_SIZE = 1024
 
@@ -23,4 +33,4 @@ PL_WEIGHT = 2
 PL_BATCH_SIZE_RATIO = 0.5
 PL_DECAY = 0.01
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
